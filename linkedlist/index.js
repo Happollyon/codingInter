9ab7401cb21d6064f3080574268ef00a9dcab288
Happollyon <36013973+Happollyon@users.noreply.github.com>
2025-02-11 22:52:50 +0000

    linked list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -49,12 +49,71 @@ class LinkedList{
             node = node.next
         }
     }
+
+    //Empties the linked list of any nodes.
+
+    clear(){
+        this.head = null
+    }
+    //Removes only the first node of the linked list. The list's head should now be the second element.
+    removeFirst(){
+        this.head = this.head.next
+    }
+    
+    //Removes the last node of the chain
+    removeLast(){
+        let count = 1
+        let size = this.size()
+        let node = this.head
+        while(node){
+            if(count === size-1 ){
+                node.next=null 
+            }if((size === 1 && node.next === null)){
+                this.head = null
+                return
+            }
+            node = node.next
+            count++
+        } 
+    }
+
+    //Inserts a new node with provided data at the end of the chain
+    insertLast(data){
+        let node = this.head
+        if(!node){
+            this.head = new Node(data,null)
+        }
+        while(node){
+            if(node.next===null){
+                node.next=new Node(data,null)
+                return
+            }
+            node = node.next
+        }
+        
+    }
+
+    //Returns the node at the provided index
+    getAt(data){
+        let count = 0;
+        let node = this.head
+        
+        while(node){
+            if(count===data){
+                return node
+            }
+            count++
+            node =node.next
+        }
+        return null
+    }
 }
 
 
-const list = new LinkedList();
-list.insertFirst('a');
-list.insertFirst('b');
-list.insertFirst('c');
-console.log(list.size()); // returns 3
-console.log(list.getFirst());
+const l = new LinkedList();
+
+    l.insertLast(1);
+    l.insertLast(2);
+    l.insertLast(3);
+    l.insertLast(4);
+    console.log(l.getAt(0).data)
