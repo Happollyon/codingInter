019e3a2286115958995771e57a0afd6d5ba8a517
Happollyon <36013973+Happollyon@users.noreply.github.com>
2025-02-11 16:24:46 +0000

    node class and linkedList class

---
+++ linkedlist/index.js
@@ -0,0 +1,14 @@
+
+
+class Node{
+    constructor(data,next){
+        this.data=data
+        this.next= next?next:null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null
+    }
+}
